ceb3fac45e1e68a2deb7940e962ebf388432c310
Brayan <brayan.rivas.perez@gmail.com>
2025-05-28 21:07:42 +0300

    Initial

---
+++ rengine/src/renderer/RRenderer.cpp
@@ -0,0 +1,8 @@
+﻿//
+// Created by Brayan on 28/05/2025.
+//
+
+#include "RRenderer.h"
+
+namespace REngine {
+} // REngine
---
+++ rengine/src/renderer/RRenderer.h
@@ -0,0 +1,9 @@
+﻿namespace REngine {
+
+class RRenderer {
+
+};
+
+} // REngine
+
+
